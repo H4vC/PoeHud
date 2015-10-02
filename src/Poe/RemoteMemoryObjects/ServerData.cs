087126efc004f2d8d8474396ad3ea84e2bf8afc2
HvC <H4vC@users.noreply.github.com>
2015-10-02 12:16:48 +0200

    Update ServerData.cs

--- src/Poe/RemoteMemoryObjects/ServerData.cs
+++ src/Poe/RemoteMemoryObjects/ServerData.cs
@@ -2,7 +2,7 @@ namespace PoeHUD.Poe.RemoteMemoryObjects
 {
     public class ServerData : RemoteMemoryObject
     {
-        public bool IsInGame => M.ReadInt(Address + 0x2FBC) == 3;
+        public bool IsInGame => M.ReadInt(Address + 0x303c) == 3;
 
         public InventoryList PlayerInventories => base.GetObject<InventoryList>(Address + 10496);
     }
